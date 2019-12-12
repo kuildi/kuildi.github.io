6375b5bb2814cb67b9844d3fe2cd42b94a4ec051
KuilDi <kuildi@yandex.ru>
2019-12-12 21:54:02 +0400

    styles

--- BaseBlocks/script.js
+++ BaseBlocks/script.js
@@ -1,3 +1,7 @@
 $('.history__show').on('click', function () {
     $(this).next('.history__hide').toggleClass('history_show');
 })
+
+$('.onoffswitch input').on('click', function () {
+    $('.theme_color_project-default').toggleClass('theme_color_project-brand');
+})
