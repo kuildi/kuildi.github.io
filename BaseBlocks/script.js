a86e3246027a6bfc38f4fb809cffe67e872eb74f
KuilDi <kuildi@yandex.ru>
2021-12-13 21:02:36 +0300

    Map app push

--- BaseBlocks/script.js
+++ BaseBlocks/script.js
@@ -1,7 +1,48 @@
-$('.history__show').on('click', function () {
-    $(this).next('.history__hide').toggleClass('history_show');
-})
+var body = document.querySelector('body');
 
-$('.onoffswitch input').on('click', function () {
-    $('.theme_color_project-default').toggleClass('theme_color_project-brand');
-})
+if (body) {
+    body.addEventListener('click', function (event) {
+        var history = event.target.closest('.history__show');
+        var onoffswitchBtn = event.target.closest('.onoffswitch input');
+    
+        if (history) {
+            history.nextElementSibling.classList.toggle('history_show');
+        }
+    
+        if (onoffswitchBtn) {
+            toggleThems();
+        }   
+    });
+}
+
+
+var defaultTheme = document.querySelectorAll('.theme_color_project-default');
+var inverseTheme = document.querySelectorAll('.theme_color_project-inverse');
+var brandTheme = document.querySelectorAll('.theme_color_project-brand');
+var warningTheme = document.querySelectorAll('.theme_color_project-warning');
+
+function toggleThems () {
+    if (defaultTheme) {
+        for (var i = 0; i < defaultTheme.length; i++) {
+            defaultTheme[i].classList.toggle('theme_color_project-brand');
+        }
+    }
+
+    if (brandTheme) {
+        for (var i = 0; i < brandTheme.length; i++) {
+            brandTheme[i].classList.toggle('theme_color_project-default');
+        }
+    }
+
+    if (inverseTheme) {
+        for (var i = 0; i < inverseTheme.length; i++) {
+            inverseTheme[i].classList.toggle('theme_color_project-warning');
+        }
+    }
+
+    if (warningTheme) {
+        for (var i = 0; i < warningTheme.length; i++) {
+            warningTheme[i].classList.toggle('theme_color_project-inverse');
+        }
+    }
+}
